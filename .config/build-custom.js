53c00177d935f317f5f9c10732b005573381071f
ng-kode <35693118+ng-kode@users.noreply.github.com>
2019-01-08 13:25:10 +0800

    Create build-custom.js

---
+++ .config/build-custom.js
@@ -0,0 +1,17 @@
+const fs = require("fs"),
+  execSync = require('child_process').execSync,
+  packages = [
+    'core',
+    'bootstrap'
+  ];
+
+// update `FORMLY-VERSION` in package.json for all sub-packages
+const version = JSON.parse(fs.readFileSync('package.json', 'utf8')).version;
+packages.map(package => {
+  const packagePath = `dist/@ngx-formly/${package}`;
+
+  execSync(`cp README.md ${packagePath}`);
+
+  package = fs.readFileSync(`${packagePath}/package.json`, 'utf8');
+  fs.writeFileSync(`${packagePath}/package.json`, package.replace(/FORMLY-VERSION/g, version));
+});
